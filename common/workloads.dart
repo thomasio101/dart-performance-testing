07c92ce7dfe67109dead1f1df039fbe77ada9209
Thomas Egbert Duursma <thomasduursma@outlook.com>
2019-02-19 20:59:35 +0100

    Equalized the number of parameters in the mixed workload function
    Updated

--- common/workloads.dart
+++ common/workloads.dart
@@ -6,6 +6,6 @@ int workloadWithNamedArguments({int x, int y}) {
   return x | y;
 }
 
-int workloadWithPositionalAndNamedArguments(int x, int y, {int z, int w}) {
-  return x | y | z | w;
+int workloadWithPositionalAndNamedArguments(int x, {int y}) {
+  return x | y;
 }
